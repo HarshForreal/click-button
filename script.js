b062e42171ca614c34e26cdb1b1bee752f03be70
HarshForreal <hp673315@gmail.com>
2024-05-17 09:16:13 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let clickCount = 0;
+
+document.getElementById('clickButton').addEventListener('click', () => {
+    clickCount++;
+    document.getElementById('clickCount').textContent = clickCount;
+    console.log('Button clicked!')
+});
